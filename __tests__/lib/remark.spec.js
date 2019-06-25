8ca70b0613ae15b087656bf3a1e9c6d8b10a11a2
Sosuke Suzuki <aosukeke@gmail.com>
2019-06-25 16:14:43 +0900

    Add tests for utils for remark

---
+++ __tests__/lib/remark.spec.js
@@ -0,0 +1,38 @@
+const { parse, stringifyFromAst } = require("../../src/lib/remark");
+
+describe("remark", () => {
+  describe("parse", () => {
+    it("returns mdast", () => {
+      // Given
+      const source = "# Foo";
+
+      // When
+      const res = parse(source);
+
+      // Then
+      expect(res).toMatchObject({
+        type: "root",
+        children: [
+          {
+            type: "heading",
+            depth: 1
+          }
+        ]
+      });
+    });
+  });
+
+  describe("stringifyFromAst", () => {
+    it("returns string", () => {
+      // Given
+      const source = "# Foo";
+      const ast = parse(source);
+
+      // When
+      const res = stringifyFromAst(ast);
+
+      // Then
+      expect(res).toBe("# Foo\n");
+    });
+  });
+});
