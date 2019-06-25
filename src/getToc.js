343c28f865c3bdc9778b0ce7e18ff78c4db854fb
Sosuke Suzuki <aosukeke@gmail.com>
2019-06-25 14:15:32 +0900

    Implement parse & stringify

--- src/getToc.js
+++ src/getToc.js
@@ -1,7 +1,28 @@
+"use strict";
+
+const unified = require("unified");
+const markdown = require("remark-parse");
+const stringify = require("remark-stringify");
+
+function parse(source) {
+  const processor = unified().use(markdown);
+  const ast = processor.runSync(processor.parse(source));
+  return ast;
+}
+
+function stringifyFromAst(ast) {
+  const processor = unified().use(stringify);
+  const text = processor.stringify(ast);
+  return text;
+}
+
 function genToc(source) {
   if (typeof source !== "string") throw new Error("source is must be a string");
 
-  return source;
+  const ast = parse(source);
+  const text = stringifyFromAst(ast);
+
+  return text;
 }
 
 module.exports = genToc;
