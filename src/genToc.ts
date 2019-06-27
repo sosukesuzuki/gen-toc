cc8fdb5a5b8f07e7d7b262f316acd19bbd72b775
Sosuke Suzuki <aosukeke@gmail.com>
2019-06-27 11:04:46 +0900

    Add the return type for genToc

--- src/genToc.ts
+++ src/genToc.ts
@@ -1,6 +1,6 @@
 import { parse, stingifyFromAst } from "./lib/remark";
 
-export default function genToc(source: string) {
+export default function genToc(source: string): string {
   const ast = parse(source);
   const text = stingifyFromAst(ast);
   return text;
